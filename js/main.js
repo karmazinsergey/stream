deabe2fb8c91d2ded1c5a49f15a69ec0a51c36a2
karmazinsergey <karmazins92@yandex.ru>
2020-06-17 23:42:36 +0300

    Add main files for project
    Созданы основные файлы для нового проекта

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Слайдер
+// $(function() {
+//     $('.fairy-tail__slider').slick({
+//         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+//         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+//         /* автоматическое проигрывание*/
+//         autoplay: true,
+//         /* скорость проигрывания */
+//         autoplaySpeed: 3000,
+//         /* анимация перелистывания слайдера */
+//         fade: true
+//     });
+//     $('.our-trip__slider').slick({
+//         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+//         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+//         /* автоматическое проигрывание*/
+//         autoplay: true,
+//         /* скорость проигрывания */
+//         autoplaySpeed: 3000,
+//         /* анимация перелистывания слайдера */
+//         fade: true
+//     });
+// });
